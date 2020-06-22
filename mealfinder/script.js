06ad777356778d00570fdf2f37b0aebdcabbc63e
michaelikang <michaelikang94@gmail.com>
2020-06-22 18:44:38 -0400

    mealfinder done

--- mealfinder/script.js
+++ mealfinder/script.js
@@ -47,8 +47,88 @@ function searchMeal (e) {
   }
 }
 
+//fetch meal by ID
+function getMealById (mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0]
+
+      addMealToDOM(meal)
+    })
+}
+
+//fetch random meal from API
+function getRandomMeal () {
+  // clear meals and headings
+  mealsEl.innerHTML = ''
+  resultHeading.innerHTML = ''
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(result => result.json())
+    .then(data => {
+      const meal = data.meals[0]
+
+      addMealToDOM(meal)
+    })
+}
+
+//Add meal to DOM
+function addMealToDOM (meal) {
+  const ingredients = []
+
+  for (i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      )
+    } else {
+      break
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${
+          meal.strCategory
+            ? `
+          <p>${meal.strCategory}</p>
+        `
+            : ''
+        }
+        ${
+          meal.strArea
+            ? `
+          <p>${meal.strArea}</p>
+        `
+            : ''
+        }
+        </div>
+        <div class="main">
+          <p>${meal.strInstructions}</p>
+          <h2>
+          Ingredients
+          </h2>
+          <ul>
+            ${ingredients
+              .map(
+                ing => `
+              <li>${ing}</li>
+            `
+              )
+              .join('')}
+          </ul>
+        </div>
+    </div> 
+  `
+}
+
 // event listeners
 submit.addEventListener('submit', searchMeal)
+random.addEventListener('click', getRandomMeal)
 
 mealsEl.addEventListener('click', e => {
   console.log(e.path)
@@ -61,4 +141,9 @@ mealsEl.addEventListener('click', e => {
   })
 
   console.log(mealInfo)
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealID')
+    getMealById(mealID)
+  }
 })
