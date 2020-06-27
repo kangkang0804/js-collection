47e17d8d78c42cb65904a13c0456790147064fbb
michaelikang <michaelikang94@gmail.com>
2020-06-27 17:34:58 -0400

    infinite scroll starter

--- expenseTracker/reverse.js
+++ expenseTracker/reverse.js
@@ -10,3 +10,12 @@ const string = 'hello'
 let reversed = reverse(string)
 
 console.log(reversed)
+
+let identifier = null
+
+if (!identifier) {
+  console.log('not true')
+}
+
+console.log("4" + 5 - "Hello world")
+console.log("42" == 42)
