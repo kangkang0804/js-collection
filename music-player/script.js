a43374681920adf545f00d0b2c71d4a3135ea23c
michaelikang <michaelikang94@gmail.com>
2020-06-27 13:37:55 -0400

    audio player complete

---
+++ music-player/script.js
@@ -0,0 +1,106 @@
+const musicContainer = document.getElementById('music-container')
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('previous')
+const nextBtn = document.getElementById('next')
+
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress-container')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
+
+//song titles
+const songs = ['hey', 'summer', 'ukulele']
+
+//keep track of songs
+let songIndex = 1
+
+//initially load song details into DOM
+loadSong(songs[songIndex])
+
+//update song details
+function loadSong (song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+//play song
+function playSong () {
+  musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play')
+  playBtn.querySelector('i.fas').classList.add('fa-pause')
+  audio.play()
+}
+
+//pause song
+function pauseSong () {
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('i.fas').classList.add('fa-play')
+  playBtn.querySelector('i.fas').classList.remove('fa-pause')
+  audio.pause()
+}
+
+//prev song
+function prevSong () {
+  songIndex--
+  if (songIndex < 0) {
+    songIndex = songs.length - 1
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+//next song
+function nextSong () {
+  songIndex++
+  if (songIndex > songs.length - 1) {
+    songIndex = 0
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+//event listeners
+playBtn.addEventListener('click', () => {
+  console.log(musicContainer.classList)
+  const isPlaying = musicContainer.classList.contains('play')
+  if (isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
+
+//update progress bar
+function updateProgress (e) {
+  const { duration, currentTime } = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+
+//set progress bar
+function setProgress (e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration
+}
+
+//change song
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+//time/song update event
+audio.addEventListener('timeupdate', updateProgress)
+
+//click on progress bar
+progressContainer.addEventListener('click', setProgress)
+
+//song ends
+audio.addEventListener('ended', nextSong)
